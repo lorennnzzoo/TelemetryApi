499738987893ae339125a067445857a8568b9835
loren <lorenbhanu@gmail.com>
2025-07-18 00:19:59 +0530

    basic crud endpoints done

---
+++ TelemtryApi/Controllers/StationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TelemtryApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StationController : ControllerBase
+    {
+        [Route("create")]
+        public IActionResult Create()
+        {
+            return Ok();
+        }
+
+        [Route("update")]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [Route("delete")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+        [Route("getall")]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [Route("get")]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+    }
+}
